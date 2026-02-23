db7dc62f75e56eda1cb71d0b1bdcbce7fea1e18e
alsrudursla <nuy0307@naver.com>
2026-02-24 04:55:06 +0900

    chore: 웹소켓 비활성화 조건 추가

--- mooney/src/main/java/org/example/service/StockWebSocketClient.java
+++ mooney/src/main/java/org/example/service/StockWebSocketClient.java
@@ -7,6 +7,7 @@ import jakarta.websocket.*;
 import lombok.RequiredArgsConstructor;
 import org.example.TradeWebSocketHandler;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,11 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 @ClientEndpoint
 @RequiredArgsConstructor
+@ConditionalOnProperty(
+    name = "websocket.enabled", // 1. application.yml에서 'websocket.enabled' 속성을 찾는다.
+    havingValue = "true", // 2. 그 값이 "true"일 때만 이 클래스를 활성화한다.
+    matchIfMissing = false // 3. 만약 속성이 없으면 (or 기본값) 비활성화한다.
+)
 public class StockWebSocketClient {
 
     private final ApprovalKeyService approvalKeyService;
